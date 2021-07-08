346eab528238150fc98535b2676be2bd8f0de36c
Vincent <vao2002@gmail.com>
2021-07-08 16:56:37 -0500

    half way finsihed with Travel slide

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var tabButtons=document.querySelectorAll(".tabContainer .buttons button");
 var tabPages=document.querySelectorAll(".tabContainer .page");
 
-function showPage(pageIndex,colorC) {
+
+function showPage(pageIndex,colorC,textColor) {
     tabButtons.forEach(function(node){
         node.style.backgroundColor="";
         node.style.color="";
     });
     tabButtons[pageIndex].style.backgroundColor=colorC;
-    tabButtons[pageIndex].style.color="black";
+    tabButtons[pageIndex].style.color=textColor;
     tabPages.forEach(function(node){
         node.style.display="none";
     });
     tabPages[pageIndex].style.display="block";
     tabPages[pageIndex].style.backgroundColor=colorC;
+    document.body.style.backgroundColor=colorC;
 }
-showPage(0,'grey');
+showPage(1,'lightblue','black');
